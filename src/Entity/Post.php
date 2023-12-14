c5ba33bff8b0a36cd979c5ec8e1c2a180538ad61
JosCousin <joshua.cousin9@gmail.com>
2023-12-14 09:54:25 +0100

    Adding a post page.

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
@@ -15,6 +16,15 @@ class Post
 
     #[ORM\Column(length: 255)]
     private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $Description = null;
+
+    #[ORM\Column]
+    private ?int $Price = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $Picture = null;
 
     public function getId(): ?int
     {
@@ -29,6 +39,42 @@ class Post
     public function setTitle(string $title): static
     {
         $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(?string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->Price;
+    }
+
+    public function setPrice(int $Price): static
+    {
+        $this->Price = $Price;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->Picture;
+    }
+
+    public function setPicture(?string $Picture): static
+    {
+        $this->Picture = $Picture;
 
         return $this;
     }
